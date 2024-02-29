a402eaccf2cd3d233f091e4e7027a35f30b7a208
fra-zen <ilya.svoevolin@yandex.ru>
2024-02-29 10:35:46 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./sass/main.scss";
+import logo from "./assets/K4C.png";
+
+const LogoImage = document.getElementById("LogoImage");
+LogoImage.src = logo;
+
+const hello = require("./js/hello");
